a8dd1d34c2f2c3e953e65e24c73494b8115c8443
Matt Phillips <spectremp24@gmail.com>
2020-11-11 17:13:36 -0500

    day3 push

---
+++ py.py
@@ -0,0 +1,57 @@
+
+# What are essential elements of programming?
+
+# What is Python? What are the beneifts of using Python?
+
+# How Python is interpereted?
+
+# How do I make choices based on user's input?
+
+# What is the diffenerece between the = operator and the == operator?
+
+# What is a Boolean value?
+
+
+# age = 23 #var called age
+
+# if (age == 25) :
+#     print(age)
+
+# name = "Matt"
+# if (name == "Matt"):
+#     print(name)
+
+# if (age <= 25):
+#     print(age)
+
+# if (age != 25):
+#     print(age)
+
+
+# count = 0
+
+# while count < 10:
+#     count += 1.25
+#     print(f"The count is {count}")
+
+# print("Done")
+
+# answer = ''
+
+# while answer != 'when':
+#     answer = input('Say when:')
+#     answer = answer.lower()
+
+# print("cheese")
+# number = ''
+# odd = 1
+# even = 2 
+
+# if number == 1:
+#     print(odd)
+# else: 
+#     print(even)
+
+# print("You\'re number is ")
+
+
---
+++ pycalc.py
@@ -0,0 +1,10 @@
+# Tip Calculator
+
+meal = float(input("Enter your bill amount:"))
+tip = float(input("How was meal?"))
+
+if 
+
+
+
+
