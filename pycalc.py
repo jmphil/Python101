7c38b1037e6426effafc2cdaadc1a087e0ba8891
Matt Phillips <spectremp24@gmail.com>
2020-11-12 10:49:36 -0500

    2nd calc partial

--- pycalc.py
+++ pycalc.py
@@ -2,17 +2,32 @@
 
 
 
-meal = float(input("Enter meal amount: "))
-tip = str(input("How was your service? "))
+# meal = float(input("Enter meal amount: "))
+# tip = str(input("How was your service? "))
 
-if tip == ('good'):
-    print(f"\nTip amount: {meal * .20:.2f}")
-elif tip == ("fair"):
-    print(f"\nTip amount: {meal * .15:.2f}")
-else :
-    print (f"\nTip amount: {meal * .10:.2f}")
+# if tip == ('good'):
+#     print(f"\nTip amount: {meal * .20:.2f}")
+# elif tip == ("fair"):
+#     print(f"\nTip amount: {meal * .15:.2f}")
+# else :
+#     print (f"\nTip amount: {meal * .10:.2f}")
 
 
 
 # print(f"\nTip amount: {tip * meal:.2f}")
 # print(f"\nTotal amount: {meal + tip * meal:.2f}")
+
+
+
+#Split Tip Calculator
+
+# meal = float(input("Enter meal amount: "))
+# tip = str(input("How was your service? "))
+# split = int(input("Split how many ways? "))
+
+# if tip == ('good'):
+#     print(f"\nTip amount: {meal * .20:.2f}")
+# elif tip == ("fair"):
+#     print(f"\nTip amount: {meal * .15:.2f}")
+# else :
+#     print (f"\nTip amount: {meal * .10:.2f}")
