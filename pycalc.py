fc69fd70a5a46b2442779ea2af7b64cb022d0d21
Matt Phillips <spectremp24@gmail.com>
2020-11-11 22:17:12 -0500

    still stuck on first calc

--- pycalc.py
+++ pycalc.py
@@ -1,10 +1,19 @@
 # Tip Calculator
 
-meal = float(input("Enter your bill amount:"))
-tip = float(input("How was meal?"))
+# good = .20
+# fair = .15
+# bad = .10
 
-if 
+meal = float(input("Enter meal amount:"))
+
+tip = int(input("How was your service?"))
 
 
 
 
+# tip_amount = int(meal * tip/100)
+# total = meal + tip
+
+
+# print(f"\nTip amount: {tip * meal:.2f}")
+# print(f"\nTotal amount: {meal + tip * meal:.2f}")
