778edb4a9008cbc0395efac72453bd958f52a001
Matt Phillips <spectremp24@gmail.com>
2020-11-13 11:33:12 -0500

    day 4 Homework

---
+++ day4hw.py
@@ -0,0 +1,55 @@
+#Multiply Vectors
+# problem [2, 4, 5] x [2, 3, 6] = [4, 12, 30]
+
+# a_list = [2, 4, 5]
+# b_list = [2 ,3, 6]
+# c_list = []
+
+# for i in range(len(a_list)):
+#     c_list.append(a_list[i] * b_list[i])
+    
+# print(c_list)
+
+# example (not my work)
+
+# list1 = [2,5,7,4]
+# list2 = [2,3,4,5]
+# multi_list = []
+# for i in range(len(list1)):
+#     multi_list.append(list1[i] * list2[i])
+# print(multi_list)
+
+#----------------------------------------------
+#Matrix Addition
+
+# nums = [ [2, -2], 
+#         [5, 3] ]`
+        
+
+#----------------------------------------------
+
+#Leet Speak
+
+#----------------------------------------------
+
+# X = 0
+# Y = 1
+# result = [[X[i][j] + Y[i][j] 
+
+#---------------------------------------------
+
+#de-dup 
+# for j in range(len(X[0]))] 
+#     for i in range(len(X))] 
+
+
+# myList = [1, 1, 1, 3, 5, 9, 0, 0]
+# newList = []
+
+# for i in myList:
+#     if i not in newList:
+#         newList.append(i) 
+
+# # myList = newList
+
+# print(newList)
