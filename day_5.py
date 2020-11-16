9d9a1badd817ff63332072f9c5237768fcf6cc42
Matt Phillips <spectremp24@gmail.com>
2020-11-16 09:57:13 -0500

    Friday HW

--- day_5.py
+++ day_5.py
@@ -1,12 +1,24 @@
 # from previous homework
 # Given a word as a string, print the result of extending any long vowels to the length of 5.
 
-dblvwl = input("Give me a word to double its long vowels: ")
-result = dblvwl
+# dblvwl = input("Give me a word to double its long vowels: ")
+# result = dblvwl
 
-for i in range(0, len(dblvwl) - 1):
-    if dblvwl[i] == dblvwl[i+1] :
-        result = dblvwl[0:i] + dblvwl[i]*3 + dblvwl[i:]
-        # break
+# for i in range(0, len(dblvwl) - 1):
+#     if dblvwl[i] == dblvwl[i+1] :
+#         result = dblvwl[0:i] + dblvwl[i]*3 + dblvwl[i:]
+#         # break
     
-print(result)
+# print(result)
+
+#Madlib function--------------------
+
+# first_name = ("")
+# subject = ("")
+
+# def info(first_name, subject)
+#     return
+
+#C to F conversion--------------------
+
+#F = (C * 9/5) + 32
