19826c556f534b3b4213c5916b22d4dd8030b443
Matt Phillips <spectremp24@gmail.com>
2020-11-18 09:59:25 -0500

    day7end

---
+++ day7hw.py
@@ -0,0 +1,32 @@
+#Friend Counter-------------------------------
+
+# Using the dictionary from the Nested dictionaries exercise above, write a function countFriends() that accepts a dictionary as an argument and returns a new dictionary that includes a new key friends_count:
+
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+
+}
+
+def countFriends(ramit)
+    friendsCount = {}
+    
+
+
+
+newDict = ()
+
+
+
+    
