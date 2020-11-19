ea56691a00e83a964fb02a6925efd99e5b43231e
Matt Phillips <spectremp24@gmail.com>
2020-11-19 11:52:04 -0500

    python class work

--- pyclass.py
+++ pyclass.py
@@ -8,19 +8,39 @@
 
 
 
-class Person: 
-    def __init__(self, name, email, phone):
-        self.name = name
-        self.email = email
-        self.phone = phone
+# class Person: 
+#     def __init__(self, name, email, phone):
+#         self.name = name
+#         self.email = email
+#         self.phone = phone
+#         self.friends = []
+#         self.add_friend = []
+#     def greet(self, other_person):
+#         print('Hello {}, I am {}!'.format(other_person.name, self.name))
 
-    def greet(self, other_person):
-        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+#     def print_contact_info(self):
+#         for friend in self.friends:
+#             print(f"{friend.name}")
+
+#     def add_friend(self, friendObj):
+#         self.friens.append(friendObj)
+
+    
+# Sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
+# Jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+# Jason = Person
 
 
-Sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
-Jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 
-Sonny.greet(Jordan)
-Jordan.greet(Sonny)
+# Sonny.print_contact_info()
+# Jordan.print_contact_info()
+# Jordan.add_friend(Jason)
+# Jordan.add_friend(Sonny)
+
+# print(len(Jordan.friends))
+
+# # Sonny.greet(Jordan)
+# Jordan.greet(Sonny)
+
+
 
